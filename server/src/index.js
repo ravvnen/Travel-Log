d8df335838fdf483973165144775f2ef8dee6622
ravvnen <phillip.boe.jensen@gmail.com>
2024-06-28 19:05:47 +0200

    setup error handling

--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const app = express();
 app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
 
-const port = 3000 || process.env.PORT;
+const port = 1337 || process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello World!'
+  });
+}
+);
+
+//Error Handling
+app.use((req, res,   next) => {
+  const error = new Error('Not Found - ${req.originalUrl}');
+  res.status(404);
+  next(error);
+}); 
+
+//Custom Error Handling - JSON response
+app.use((error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : error.stack,
+  });
+}
+);
+
+
+
 
 
