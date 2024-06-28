ba6434b4a3e6cb4635d89b9f9a5ad965eb8e35e0
ravvnen <phillip.boe.jensen@gmail.com>
2024-06-28 21:05:49 +0000

    mongoose setup and install dotenv

--- server/src/models/LogEntry.js
+++ server/src/models/LogEntry.js
@@ -21,8 +21,16 @@ const logEntrySchema = new Schema({
     max: [10, 'Rating must be below 10'],
     default: 0
   },
-  latitude: requiredNumber,
-  longitude: requiredNumber,
+  latitude:{
+    ...requiredNumber,
+    min: [-90, 'Latitude must be between -90 and 90'],
+    max: [90, 'Latitude must be between -90 and 90'],
+    },
+  longitude: {
+    ...requiredNumber,
+    min: [-180, 'Longitude must be between -180 and 180'],
+    max: [180, 'Longitude must be between -180 and 180'],
+  },
   visitDate: {
     required: true,
     type: Date,
