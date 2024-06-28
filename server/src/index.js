f995cc1448aed55ab5a734a201d3ff6d15b1cfd9
ravvnen <phillip.boe.jensen@gmail.com>
2024-06-28 16:04:11 +0000

    adding

---
+++ server/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('common'));
+
+const port = 3000 || process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
